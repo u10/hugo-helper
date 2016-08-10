da91de31b91a3ee980d0dd4fc4db8e054ff36a8f
sulj <vaniship@gmail.com>
2016-08-10 11:34:41 +0800

    根据组件修改调整代码,集中业务逻辑

--- wwwroot/src/App.vue
+++ wwwroot/src/App.vue
@@ -1,53 +1,258 @@
 <template>
-  <x-frame :content="currentView" :gutter-size="5">
-    <menu slot="menu"
-          :model="mainMenu"
-          @menu-click="mainMenuClick"></menu>
+  <x-frame :gutter-size="5">
+    <menu slot="menu"></menu>
     <tree class="border"
-          slot="left"
-          :menu="treeMenu.data"
-          :data="window.explorer.data"
-          @menu-click="treeMenuClick"
-          @item-dblclick="treeItemDblClick"></tree>
+          slot="left" v-ref:tree-view></tree>
     <component slot="right"
                :is="rightView"
-               v-ref:right-view
-               :content-provider="contentProvider"
-               @save="onSave"></component>
+               v-ref:right-view></component>
     <console slot="bottom" :id="'hugo'"></console>
     <state slot="state"></state>
   </x-frame>
 </template>
 
 <script>
+  import _ from 'lodash'
   import {hugo} from './lib/ws'
-  import store from './vuex/store'
-  import {window} from './vuex/getters'
-  import {setExplorerData} from './vuex/actions'
-  import $ from 'jquery'
+  import fileTypes from './lib/fileTypes'
+  import fileManager from './lib/fileManager'
+//  import store from './vuex/store'
   import XFrame from './components/XFrame'
-  import Editor from './components/Editor'
-  import Menu from './components/Menu'
+  import BaseCodeMirror from './components/CodeMirror'
+  import BaseEditorManager from './components/EditorManager'
+  import BaseTree from './components/Tree'
+  import BaseMenu from './components/Menu'
   import Console from './components/Console'
   import State from './components/State'
-  import Tree from './components/Tree'
-  import {Base64} from 'js-base64'
   import swal from './lib/vue-sweetalert'
-  export default {
-    store,
-    vuex: {
-      getters: {
-        window
+
+  function extend (base, option) {
+    const ret = _.clone(base)
+    for (let key in option) {
+      if (_.isFunction(option[key])) {
+        ret[key] = option[key]
+      } else {
+        ret[key] = _.extend(ret[key], option[key])
+      }
+    }
+    return ret
+  }
+
+  // 配置CoreMirror
+  import '../../node_modules/codemirror/addon/search/search'
+  import '../../node_modules/codemirror/addon/search/match-highlighter'
+  import '../../node_modules/codemirror/addon/dialog/dialog.css'
+  import '../../node_modules/codemirror/mode/meta'
+  import '../../node_modules/codemirror/mode/htmlmixed/htmlmixed'
+  import '../../node_modules/codemirror/mode/gfm/gfm'
+  import '../../node_modules/codemirror/mode/toml/toml'
+  const CodeMirror = extend(BaseCodeMirror, {
+    methods: {
+      getConfig () {
+        return {
+          lineNumbers: true,
+          extraKeys: {
+            'Alt-F': 'findPersistent'
+          },
+          highlightSelectionMatches: {
+            showToken: /\w/,
+            annotateScrollbar: true
+          }
+        }
+      }
+    }
+  })
+
+  // 配置编辑器
+  const EditorManager = extend(BaseEditorManager, {
+    methods: {
+      doSave (data) {
+        fileManager.save(
+          data.path,
+          data.draft,
+          function () {
+            data.content = data.draft
+            data.draft = null
+            console.log('save done!')
+          },
+          function () {
+            console.log('save failed!')
+          })
       },
-      actions: {
-        setExplorerData
+      getComponentType (path) {
+        if (/\.(html|htm|xml|json|js|css|md|toml)$/i.test(path)) {
+          return 'CodeMirror'
+        }
+      },
+      getContent (path, done) {
+        if (_.isUndefined(this.getComponentType(path))) {
+          done('')
+        } else {
+          fileManager.cat(path, done)
+        }
       }
     },
+    components: {
+      CodeMirror
+    }
+  })
+
+  // 配置Tree
+  function treeMenus (node) {
+    const data = node.original
+    return {
+      open: {
+        label: '查看',
+        _disabled: data.type === '.'
+      },
+      add: {
+        label: '新建'
+      },
+      rename: {
+        label: '重命名'
+      },
+      del: {
+        label: '删除'
+      }
+    }
+  }
+  const Tree = extend(BaseTree, {
+    methods: {
+      menuClick (evt) {
+        const self = this
+        switch (evt.action) {
+          case 'open':
+            this.$dispatch('cmd-file-open', evt.jstree.get_path(evt.node).join('/'))
+            break
+          case 'add':
+            swal({
+              title: '新建文件',
+              text: '请输入文件名:',
+              type: 'input',
+              inputType: 'text',
+              showCancelButton: true,
+              closeOnConfirm: false
+            }, function (filename) {
+              if (filename !== false) {
+                const paths = evt.jstree.get_path(evt.node)
+                paths.push(filename)
+                const path = paths.join('/')
+                fileManager.save(
+                  path,
+                  '',
+                  function () {
+                    swal('创建成功', '文件创建成功!', 'success')
+                    self.$dispatch('cmd-file-open', path)
+                    self.$dispatch('cmd-refresh-tree')
+                  },
+                  function () {
+                    swal.showInputError('创建文件失败.')
+                  })
+              }
+            })
+            break
+          case 'rename':
+            {
+              const paths = evt.jstree.get_path(evt.node)
+              const oldFileName = paths.pop()
+              const path = paths.join('/')
+              swal({
+                title: '文件名重命名',
+                text: '请输入新的文件名:',
+                type: 'input',
+                inputType: 'text',
+                inputValue: oldFileName,
+                showCancelButton: true,
+                closeOnConfirm: false
+              }, function (filename) {
+                if (filename !== false) {
+                  const oldPath = `${path}/${oldFileName}`
+                  const newPath = `${path}/${filename}`
+                  fileManager.mv(
+                    oldPath,
+                    newPath,
+                    function () {
+                      swal('重命名成功', '文件重命名成功!', 'success')
+                      self.$dispatch('cmd-file-rename', oldPath, newPath)
+                      self.$dispatch('cmd-refresh-tree')
+                    },
+                    function () {
+                      swal.showInputError('重命名文件失败.')
+                    }
+                  )
+                }
+              })
+              break
+            }
+          case 'del':
+            {
+              const paths = evt.jstree.get_path(evt.node)
+              const path = paths.join('/')
+              swal({
+                title: '删除文件',
+                text: `确定要删除如下文件吗(无法恢复):\n${path}`,
+                type: 'warning',
+                showCancelButton: true,
+                closeOnConfirm: false
+              }, function () {
+                fileManager.rm(
+                  path,
+                  function () {
+                    swal('删除成功', '文件删除成功!', 'success')
+                    self.$dispatch('cmd-file-close', path)
+                    self.$dispatch('cmd-refresh-tree')
+                  },
+                  function () {
+                    swal('删除成功', '文件删除失败!', 'error')
+                  })
+              })
+              break
+            }
+        }
+      },
+      itemDblClick (evt) {
+        const data = evt.data
+        if (data.type !== '.') {
+          this.$dispatch('cmd-file-open', evt.jstree.get_path(evt.node).join('/'))
+        }
+      },
+      getConfig () {
+        const self = this
+        return {
+          contextmenu: {
+            items (node, cb) {
+              const menus = treeMenus(node)
+              for (let action in menus) {
+                let menu = menus[action]
+                menu.action = function () {
+                  self.menuClick({
+                    action: action,
+                    data: node.original,
+                    node: node,
+                    jstree: self.jstree
+                  })
+                }
+              }
+              return menus
+            }
+          },
+          sort (a, b) {
+            a = this.get_node(a)
+            b = this.get_node(b)
+            a = a.original.type === '.' ? '\0' + a.text : a.text
+            b = b.original.type === '.' ? '\0' + b.text : b.text
+            return a > b ? 1 : -1
+          },
+          'types': fileTypes,
+          plugins: ['search', 'contextmenu', 'sort', 'types']
+        }
+      }
+    }
+  })
+  const Menu = extend(BaseMenu, {
     data () {
       return {
-        rightView: '',
-        projectPath: '.',
-        mainMenu: [
+        menus: [
           {
             name: '文件',
             items: {
@@ -96,136 +301,11 @@
               }
             }
           }
-        ],
-        treeMenu: {
-          data (node) {
-            const data = node.original
-            return {
-              open: {
-                label: '查看',
-                _disabled: data.type === '+'
-              },
-              add: {
-                label: '新建'
-              },
-              rename: {
-                label: '重命名'
-              },
-              del: {
-                label: '删除'
-              }
-            }
-          }
-        }
+        ]
       }
     },
     methods: {
-      contentProvider (path, done) {
-        $.ajax({
-          url: '/cli/file/' + encodeURIComponent('get -p ' + path),
-          success (ret) {
-            done(ret.data)
-          }
-        })
-      },
-      onSave (data) {
-        $.ajax({
-          url: '/cli/file/' + encodeURIComponent(`set -p ${data.path} -d ${Base64.encode(data.draft)}`),
-          complete (xhr, status) {
-            if (status === 'success') {
-              data.content = data.draft
-              data.draft = null
-              console.log('save done!')
-            } else {
-              console.log('save failed')
-            }
-          }
-        })
-      },
-      treeMenuClick (evt) {
-        const self = this
-        switch (evt.action) {
-          case 'open':
-            this.openFile(evt.jstree.get_path(evt.node).join('/'))
-            break
-          case 'add':
-            swal({
-              title: '新建文件',
-              text: '请输入文件名:',
-              type: 'input',
-              inputType: 'text',
-              showCancelButton: true,
-              closeOnConfirm: false
-            }, function (filename) {
-              if (filename !== false) {
-                const paths = evt.jstree.get_path(evt.node)
-                const path = paths.join('/')
-                $.ajax({
-                  url: '/cli/file/' + encodeURIComponent(`add -p ${path}/${filename}`)
-                }).done(function (data) {
-                  swal('创建成功', '文件创建成功!', 'success')
-                  self.openFile(`${path}/${filename}`)
-                  self.refreshTree()
-                }).error(function (data) {
-                  swal.showInputError('创建文件失败.')
-                })
-              }
-            })
-            break
-          case 'rename':
-            {
-              const paths = evt.jstree.get_path(evt.node)
-              const oldFileName = paths.pop()
-              const path = paths.join('/')
-              swal({
-                title: '文件名重命名',
-                text: '请输入新的文件名:',
-                type: 'input',
-                inputType: 'text',
-                inputValue: oldFileName,
-                showCancelButton: true,
-                closeOnConfirm: false
-              }, function (filename) {
-                if (filename !== false) {
-                  $.ajax({
-                    url: '/cli/file/' + encodeURIComponent(`rename -p ${path}/${oldFileName} -d ${path}/${filename}`)
-                  }).done(function (data) {
-                    swal('重命名成功', '文件重命名成功!', 'success')
-                    self.renameFile(`${path}/${oldFileName}`, `${path}/${filename}`)
-                    self.refreshTree()
-                  }).error(function (data) {
-                    swal.showInputError('重命名文件失败.')
-                  })
-                }
-              })
-              break
-            }
-          case 'del':
-            {
-              const paths = evt.jstree.get_path(evt.node)
-              const path = paths.join('/')
-              swal({
-                title: '删除文件',
-                text: `确定要删除如下文件吗(无法恢复):\n${path}`,
-                type: 'warning',
-                showCancelButton: true,
-                closeOnConfirm: false
-              }, function () {
-                $.ajax({
-                  url: '/cli/file/' + encodeURIComponent(`del -p ${path}`)
-                }).done(function (data) {
-                  swal('删除成功', '文件删除成功!', 'success')
-                  self.closeFile(path)
-                  self.refreshTree()
-                }).error(function (data) {
-                  swal('删除成功', '文件删除失败!', 'error')
-                })
-              })
-              break
-            }
-        }
-      },
-      mainMenuClick (key) {
+      menuClick (key) {
         switch (key) {
           case 'start':
             hugo.start({
@@ -239,75 +319,90 @@
             hugo.stop()
             break
           case 'undo':
-            this.$broadcast('cmd-undo')
+            this.getEditor().undo()
             break
           case 'redo':
-            this.$broadcast('cmd-redo')
+            this.getEditor().redo()
             break
           case 'save':
-            this.$broadcast('cmd-save')
+            this.getEditor().save()
             break
           case 'about':
             swal('Hugo Helper', 'MIT License\nv0.0.1')
             break
         }
         console.log(key)
-      },
-      treeItemDblClick (evt) {
-        const data = evt.data
-        if (data.type !== '+') {
-          this.openFile(evt.jstree.get_path(evt.node).join('/'))
-        }
-      },
+      }
+    }
+  })
+
+  export default {
+//    store,
+//    vuex: {
+//      getters: {
+//      },
+//      actions: {
+//      }
+//    },
+    data () {
+      return {
+        rightView: '',
+        projectPath: '.'
+      }
+    },
+    methods: {
       refreshTree (projectPath) {
         if (projectPath && (projectPath !== this.projectPath)) {
           this.projectPath = projectPath
         }
         const self = this
-        $.ajax({
-          url: '/cli/dir/' + encodeURIComponent(`list -p ${this.projectPath} -d 32`),
-          success (data) {
-            self.setExplorerData(data.data)
-          }
+        fileManager.ls(this.projectPath, function (data) {
+          self.$refs.treeView.refresh(data)
         })
       },
-      openFile (path) {
-        this.rightView = 'Editor'
-        const self = this
-        this.$nextTick(function () {
-          self.$refs.rightView.open(path)
-        })
-      },
-      renameFile (oldPath, newPath) {
-        this.rightView = 'Editor'
-        const self = this
-        this.$nextTick(function () {
-          self.$refs.rightView.rename(oldPath, newPath)
-        })
-      },
-      closeFile (path) {
-        this.rightView = 'Editor'
-        const self = this
-        this.$nextTick(function () {
-          self.$refs.rightView.close(path)
-        })
+      getEditor () {
+        return this.$refs.rightView
       }
     },
-    watch: {},
     ready () {
       this.refreshTree()
       const self = this
       hugo.addConsoleListener(function (data) {
         self.$broadcast('console-hugo', data)
       })
+
+      this.$on('cmd-file-open', function (path) {
+        self.rightView = 'EditorManager'
+        this.$nextTick(function () {
+          self.getEditor().open(path)
+        })
+      })
+      this.$on('cmd-file-close', function (path) {
+        self.rightView = 'EditorManager'
+        this.$nextTick(function () {
+          self.getEditor().close(path)
+        })
+      })
+      this.$on('cmd-file-rename', function (oldPath, newPath) {
+        self.rightView = 'EditorManager'
+        this.$nextTick(function () {
+          self.getEditor().rename(oldPath, newPath)
+        })
+      })
+      this.$on('cmd-file-save', function () {
+        self.getEditor().save()
+      })
+      this.$on('cmd-refresh-tree', function () {
+        self.refreshTree()
+      })
     },
     components: {
       XFrame,
       Menu,
-      Editor,
+      Tree,
+      EditorManager,
       Console,
-      State,
-      Tree
+      State
     }
   }
 </script>
@@ -331,5 +426,13 @@
     padding: 0;
     margin: 0;
     border: grey;
+  }
+
+  .cm-matchhighlight {
+    background-color: lightgreen
+  }
+
+  .CodeMirror-selection-highlight-scrollbar {
+    background-color: green
   }
 </style>
