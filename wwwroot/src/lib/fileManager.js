ed4f050bb1236e8609f1a18772ba2b19b52373c7
sulj <vaniship@gmail.com>
2016-08-10 11:28:40 +0800

    抽离文件操作相关代码

---
+++ wwwroot/src/lib/fileManager.js
@@ -0,0 +1,45 @@
+import $ from 'jquery'
+import {Base64} from 'js-base64'
+export default {
+  ls (path, done, error) {
+    $.ajax({
+      url: '/cli/dir/' + encodeURIComponent(`list -p ${path} -d 32`)
+    })
+      .done(function (data) {
+        done(data.data)
+      })
+      .error(error)
+  },
+  mkDir (path, done, error) {
+  },
+  cat (path, done, error) {
+    $.ajax({
+      url: '/cli/file/' + encodeURIComponent('get -p ' + path)
+    })
+      .done(function (data) {
+        done(data.data)
+      })
+      .error(error)
+  },
+  save (path, data, done, error) {
+    $.ajax({
+      url: '/cli/file/' + encodeURIComponent(`set -p ${path} -d ${Base64.encode(data)}`)
+    })
+      .done(done)
+      .error(error)
+  },
+  mv (oldPath, newPath, done, error) {
+    $.ajax({
+      url: '/cli/file/' + encodeURIComponent(`rename -p ${oldPath} -d ${newPath}`)
+    })
+      .done(done)
+      .error(error)
+  },
+  rm (path, done, error) {
+    $.ajax({
+      url: '/cli/file/' + encodeURIComponent(`del -p ${path}`)
+    })
+      .done(done)
+      .error(error)
+  }
+}
