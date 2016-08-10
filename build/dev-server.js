c7d6fc75f685c05e5e4f789c2e1059ca26b93e83
sulj <vaniship@gmail.com>
2016-08-10 11:19:03 +0800

    修复静态目录服务路径错误的问题

--- build/dev-server.js
+++ build/dev-server.js
@@ -57,7 +57,7 @@ app.use(hotMiddleware)
 
 // serve pure static assets
 var staticPath = path.posix.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+app.use(staticPath, express.static(path.join(__dirname, '..', 'wwwroot', 'static')))
 
 module.exports = app.listen(port, function (err) {
   if (err) {
